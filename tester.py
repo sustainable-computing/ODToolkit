73c7a2884ba54b11c7926c7f0baeb4582dea12c2
Tianyu Zhang <skyu0221@gmail.com>
2018-12-23 19:05:23 -0700

    Update a sample analyze result

--- tester.py
+++ tester.py
@@ -3,7 +3,5 @@ import numpy as np
 
 dataset = odtk.data.import_data("datatest.csv", time_column=1)
 print(type(dataset))
-# odtk.analyzer.analyze(dataset, 65, "result.txt")
-odtk.modifier.downsample(dataset, 120)
-odtk.modifier.change_to_binary(dataset)
+odtk.analyzer.analyze(dataset, 65, "result.txt")
 # print(dataset)
