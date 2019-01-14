20050b6a3fd46d599959e03394c21d442b12d511
Tianyu Zhang <skyu0221@gmail.com>
2019-01-13 20:47:10 -0700

    Update SVM/SVMDA

--- odtk/model/support_vector_machine.py
+++ odtk/model/support_vector_machine.py
@@ -1,25 +1,51 @@
 from sklearn import svm
-from odtk.data.dataset import Dataset
+# Have to include this
+from odtk.model.superclass import *
 
 
-def support_vector_machine(train,
-                           test,
-                           retrain=None,
-                           gamma='auto',
-                           kernel='linear',
-                           penalty_error=1):
+class SVM(NormalModel):
+    # For NormalModel, require two parameters: train and test
+    # For DomainAdaptiveModel, require three parameters: source, target_retrain and target_test
+    def __init__(self, train, test):
+        # all changeable parameters now store as an editable instance
+        self.train = train
+        self.test = test
+        self.gamma = 'auto',
+        self.kernel = 'linear',
+        self.penalty_error = 1
 
-    if not isinstance(train, Dataset) or not isinstance(test, Dataset) or \
-            retrain is not None and not isinstance(retrain, Dataset):
-        raise ValueError("Given train and test is not class odtk.data.dataset.Dataset")
+    # the model must have a method called run, and return the predicted result
+    def run(self):
+        classifier = svm.SVC(kernel=self.kernel, C=self.penalty_error, gamma=self.gamma)
 
-    classifier = svm.SVC(kernel=kernel, C=penalty_error, gamma=gamma)
+        classifier.fit(self.train.data, self.train.occupancy)
 
-    classifier.fit(train.data, train.occupancy)
+        predict_occupancy = classifier.predict(self.test.data)
 
-    if retrain is not None:
-        classifier.fit(retrain.data, retrain.occupancy)
+        return predict_occupancy
 
-    predict_occupancy = classifier.predict(test.data)
 
-    return predict_occupancy
+class SVMDA(DomainAdaptiveModel):
+    # For NormalModel, require two parameters: train and test
+    # For DomainAdaptiveModel, require three parameters: source, target_retrain and target_test
+    def __init__(self, source, target_retrain, target_test):
+        # all changeable parameters now store as an editable instance
+        self.source = source
+        self.target_retrain = target_retrain
+        self.target_test = target_test
+        self.gamma = 'auto',
+        self.kernel = 'linear',
+        self.penalty_error = 1
+
+    # the model must have a method called run, and return the predicted result
+    def run(self):
+        classifier = svm.SVC(kernel=self.kernel, C=self.penalty_error, gamma=self.gamma)
+
+        classifier.fit(self.source.data, self.source.occupancy)
+
+        if self.target_retrain is not None:
+            classifier.fit(self.target_retrain.data, self.target_retrain.occupancy)
+
+        predict_occupancy = classifier.predict(self.target_test.data)
+
+        return predict_occupancy
