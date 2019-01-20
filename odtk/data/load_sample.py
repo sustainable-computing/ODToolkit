5df609f34b91d92249030753118a3ce762057b10
Tianyu Zhang <skyu0221@gmail.com>
2019-01-20 14:08:38 -0700

    Update dataset

--- odtk/data/load_sample.py
+++ odtk/data/load_sample.py
@@ -1,31 +1,12 @@
 from odtk.data.read import read
+from os.path import abspath, join, basename, isfile
+import os
 
 
 def load_sample(sample_name):
+    sample_dir = join(abspath(__file__).rstrip(basename(__file__)), "sample_csv")
     sample_name = sample_name.split('-')
-    if sample_name[0] == "umons":
-        if sample_name[1] == "datatest":
-            return read("./odtk/data/sample_csv/umons/datatest")
-        elif sample_name[1] == "datatest2":
-            return read("./odtk/data/sample_csv/umons/datatest2")
-        elif sample_name[1] == "datatraining":
-            return read("./odtk/data/sample_csv/umons/datatraining")
-        elif sample_name[1] == "all":
-            return read("./odtk/data/sample_csv/umons/umons")
-
-    if sample_name[0] == "sdu":
-        if sample_name[1] == "508":
-            return read("./odtk/data/sample_csv/sdu/508")
-        elif sample_name[1] == "511":
-            return read("./odtk/data/sample_csv/sdu/511")
-        elif sample_name[1] == "601":
-            return read("./odtk/data/sample_csv/sdu/601")
-        elif sample_name[1] == "604":
-            return read("./odtk/data/sample_csv/sdu/604")
-        elif sample_name[1] == "all":
-            return read("./odtk/data/sample_csv/sdu/sdu")
-
-    if sample_name[0] == "aifb":
-        return read("./odtk/data/sample_csv/aifb/aifb")
-
+    directory = join(sample_dir, *sample_name)
+    if isfile(directory):
+        return read(directory)
     print("Data not defined")
--- tester.py
+++ tester.py
@@ -1,11 +1,18 @@
 import odtk
 import numpy as np
+from tqdm import tqdm
 
-# odtk.data.write(odtk.data.import_data("./odtk/data/sample_csv/aifb/data.csv", 0, room_name="data"), "data")
+# for i in tqdm(range(1, 25)):
+#     odtk.data.write(
+#         odtk.data.import_data("./odtk/data/sample_csv/lbl/data" + str(i) + ".csv", 0, room_name="data_" + str(i)),
+#         "data" + str(i))
 
-# odtk.data.write(odtk.data.load_sample("aifb") + odtk.data.load_sample("umons-datatest2") + odtk.data.load_sample("umons-datatraining"), "umons")
+all = odtk.data.load_sample("lbl-data1")
+for i in tqdm(range(2, 25)):
+    all += odtk.data.load_sample("lbl-data" + str(i))
+odtk.data.write(all, "all")
 
-# odtk.analyzer.analyze(odtk.data.load_sample("aifb"), 65, save_file="result.txt")
+# odtk.analyzer.analyze(odtk.data.load_sample("niom-all"), 65, save_file="result.txt")
 
 for i in range(0):
     umons = odtk.data.load_sample("umons-all")
