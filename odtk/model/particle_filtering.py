0effc604934724e33035fc832e6ea7e6b1180e46
Tianyu Zhang <skyu0221@gmail.com>
2019-01-24 13:37:12 -0700

    Update particle_filtering.py

--- odtk/model/particle_filtering.py
+++ odtk/model/particle_filtering.py
@@ -9,48 +9,44 @@ class PF(NormalModel):
     def __init__(self,
                  train,
                  test):
-
         self.train = train
         self.test = test
         self.number_of_hidden_states = int(np.amax(train.occupancy)) + 1
 
     def run(self):
-    
         hmm = hmm_core.HMM_Core(number_of_hidden_states=self.number_of_hidden_states)
-        hmm.learn(hidden_seq=np.array(self.train.occupancy, int).flatten(), 
+        hmm.learn(hidden_seq=np.array(self.train.occupancy, int).flatten(),
                   emission_seq=self.train.data)
 
         predict_occupancy = hmm.pf_predict(emission_seq=np.array(self.test.data))
 
-        return np.reshape(predict_occupancy, (-1,1))
-        
+        return np.reshape(predict_occupancy, (-1, 1))
+
+
 class PF_DA(DomainAdaptiveModel):
 
     def __init__(self,
                  source,
                  target_retrain,
                  target_test):
-
         self.source = source
         self.target_retrain = target_retrain
         self.target_test = target_test
         self.number_of_hidden_states = int(np.amax(source.occupancy)) + 1
 
-            
     def run(self):
-
         hmm_source = hmm_core.HMM_Core(number_of_hidden_states=self.number_of_hidden_states)
 
-        hmm_source.learn(hidden_seq=np.array(self.source.occupancy, int).flatten(), 
-                  emission_seq=self.source.data)
+        hmm_source.learn(hidden_seq=np.array(self.source.occupancy, int).flatten(),
+                         emission_seq=self.source.data)
 
         hmm_target = hmm_core.HMM_Core(number_of_hidden_states=self.number_of_hidden_states)
 
         hmm_target.prior(hmm_source)
 
-        hmm_target.learn(hidden_seq=np.array(self.target_retrain.occupancy, int).flatten(), 
-                  emission_seq=self.target_retrain.data)
+        hmm_target.learn(hidden_seq=np.array(self.target_retrain.occupancy, int).flatten(),
+                         emission_seq=self.target_retrain.data)
 
-        predict_occupancy = hmm.pf_predict(emission_seq=np.array(self.target_test.data))
+        predict_occupancy = hmm_target.pf_predict(emission_seq=np.array(self.target_test.data))
 
-        return np.reshape(predict_occupancy, (-1,1))
+        return np.reshape(predict_occupancy, (-1, 1))
